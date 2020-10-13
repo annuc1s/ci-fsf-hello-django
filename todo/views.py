6a3cb99574d714be2ee5e3652e7a9dd9d7aa19e4
annuc1s <annuciis121@gmail.com>
2020-10-13 13:34:57 +0000

    adding a templates folder within the app folder

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 #takes a http request from the user and return a http response to the user that says hello
 def say_hello(request):
-    return HttpResponse("Hello!")
+    return HttpResponse("<h1>Hello!</h1><p>This is a paragraph.</p>")
